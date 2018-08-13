f744b48825054beb59a7d07c2c764f336bc360d4
Nibbit99 <42346943+Nibbit99@users.noreply.github.com>
2018-08-13 16:44:18 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,6 +9,9 @@ client.on('message', message => {
     if (message.content === 'hb!neef') {
     	message.reply('Broeder!');
   	}
+    if (message.content === 'hb!henk') {
+    	message.reply('http://www.voetbalclub2000.nl/tm2010/images/uploads/players/2014/01/2666_79785_1390156605_tixhr-552172661.png');
+  	}
 });
 
 // THIS  MUST  BE  THIS  WAY
