1c95b55d214a9f30c9c86dd8c4064d30fb0840ff
Nibbit99 <42346943+Nibbit99@users.noreply.github.com>
2018-08-13 17:20:27 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,14 +7,10 @@ client.on('ready', () => {
 
 client.on('message', message => {
     if (message.content.includes('neef')) {
-    	message.reply('Broeder!');
+    	message.reply('neef!');
   	}
     if (message.content === 'hb!henk') {
     message.channel.send("Hank Borgerson has arrived!", {files: ["https://i.imgur.com/cr0cy6X.png"]});
-  	}
-    if (message.content === 'ownernibbit!gamestatus') {
-    bot.user.setGame('with himself')
-    message.reply('Vader, mijn game status is geupdate!');
   	}
 });
 
