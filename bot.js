adf4966ba69ed2287914c818ede4dbaed8e57ab2
Nibbit99 <42346943+Nibbit99@users.noreply.github.com>
2018-08-13 17:22:29 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,7 +7,7 @@ client.on('ready', () => {
 
 client.on('message', message => {
     if (message.content.includes('neef')) {
-    	message.reply('neef!');
+    	message.reply('Broeder!');
   	}
     if (message.content === 'hb!henk') {
     message.channel.send("Hank Borgerson has arrived!", {files: ["https://i.imgur.com/cr0cy6X.png"]});
