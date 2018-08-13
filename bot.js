1f49626dfc385c73735d68713525bf61c92005d3
Nibbit99 <42346943+Nibbit99@users.noreply.github.com>
2018-08-13 20:33:37 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,9 +7,17 @@ client.on('ready', () => {
 
 client.on('message', message => {
     if (message.content.includes('neef')) {
-    var messagelower = message.toLowerCase();
-    	message.reply(messagelower);
+    	message.reply("Ja broer?");
   	}
+    
+    if (message.content.includes('hallo henk')) {
+    	message.reply("hallo daar mijn naam is zeker Henk. Wees gegroet en heb een goede dag, want god wees met u.");
+  	}
+    if (message.content.includes('borger')) {
+    	message.reply("Ja? Mijn bijnaam is inderdaad borger, problemen mee? Moet je vechten? Komen dan, nu! Ik sta voor jouw huis kom nu naar buiten dan. Komen dan! Echt ik heb er genoeg van. Ik pik het niet meer, ik klap jou nu! Hier voor jouw huis, komen dan broer. Jij zoekt echt ruzie he neef!");
+  	}
+    
+    
     if (message.content === 'hb!henk') {
     message.channel.send("Hank Borgerson has arrived!", {files: ["https://i.imgur.com/cr0cy6X.png"]});
   	}
